fcb5df61df1f78cf72a485f5f67e8a30e5d9e44d
camon <camon@nexon.co.kr>
2017-07-10 18:01:25 +0900

    불필요 소스 제거

--- lagomhello-api/src/main/java/com/camon/lagomhello/api/LagomhelloEvent.java
+++ lagomhello-api/src/main/java/com/camon/lagomhello/api/LagomhelloEvent.java
@@ -3,29 +3,11 @@
  */
 package com.camon.lagomhello.api;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.google.common.base.Preconditions;
-import lombok.Value;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
-//@JsonSubTypes({
-//    @JsonSubTypes.Type(value = LagomhelloEvent.GreetingMessageChanged.class, name = "greeting-message-changed")
-//})
 public interface LagomhelloEvent {
 
   String getName();
 
-//  @Value
-//  final class GreetingMessageChanged implements LagomhelloEvent {
-//    public final String name;
-//    public final String message;
-//
-//    @JsonCreator
-//    public GreetingMessageChanged(String name, String message) {
-//      this.name = Preconditions.checkNotNull(name, "name");
-//      this.message = Preconditions.checkNotNull(message, "message");
-//    }
-//  }
 }
